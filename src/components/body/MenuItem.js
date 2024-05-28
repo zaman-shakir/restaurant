4a90622d645979c031824b940811f943a41252d0
Shakir Uz Zaman <zaman.shakirdev@gmail.com>
2024-05-29 02:35:49 +0600

    Feature: Designing menu items in lists

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import { Card, CardImg, CardImgOverlay, CardBody, CardTitle} from 'reactstrap';
 const MenuItem = (props) =>{
     console.log(props);
     return (
         <div>
+            <Card>
+                <CardBody>
+                    <CardImg
+                        width="100%"
+                        alt={props.dish.name}
+                        src={props.dish.image}
+                        style={{opacity:"0.5"}}
+                      />
+                    <CardImgOverlay>
+                        <CardTitle style={{color:"white", fontSize:"32px" }}>{props.dish.name}</CardTitle>
+                    </CardImgOverlay>
 
+                </CardBody>
+            </Card>
         </div>
     );
 
