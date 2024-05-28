2e0fca16e0f4cc3487154b11710b70c14ebba5f6
Shakir Uz Zaman <zaman.shakirdev@gmail.com>
2024-05-29 00:42:14 +0600

    Component Tree

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import React from 'react';
-
+import MainComponent from './components/MainComponent';
 function App() {
   return (
     <div className="App">
-
+        <MainComponent />
     </div>
   );
 }
